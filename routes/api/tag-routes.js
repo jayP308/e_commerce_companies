bd502b28966cfb1d52a50170f77a457ddc16a586
Junnel Padilla <junnelpadilla307@gmail.com>
2023-06-30 04:36:01 -0700

    Added all the routes to tags model

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -27,16 +27,65 @@ router.get('/:id', async(req, res) => {
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new tag
+  try {
+    const createTag = await Tag.create({
+      tag_name: req.body.tag_name,
+    });
+
+    res.status(500).json(createTag);
+  } catch (err) {
+    res.status(500).json(err);
+    return;
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async(req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const id = req.params.id;
+    const tagUpdate = await Tag.update({
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: {
+        id: id,
+      }
+    });
+
+    if(tagUpdate[0] === 0) {
+      res.status(404).json({ error: 'Cannot Find Tag, Please Try Again!'})
+    } else {
+      res.status(200).json(tagUpdate);
+    }   
+  } catch (err) {
+    res.status(500).json(err);
+    return;
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const id = req.params.id;
+
+    const deleteTag = await Tag.destroy({
+      where: {
+        id: id,
+      }
+    });
+
+    if (deleteTag === 0) {
+      res.status(404).json({ error: 'Category Does Not Exist!'})
+    } else {
+      console.log('Product Deleted Successfully!')
+      res.status(200).json(deleteTag);
+    }
+  } catch (err){
+    res.status(500).json(err);
+    return;
+  }
 });
 
 module.exports = router;
